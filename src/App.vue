f380bb3f34c09a1f574ce427ccdb1b02b5c2bf67
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2023-11-07 15:53:46 -0600

    Setting up Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
-import TasksPage from './pages/TasksPage.vue';
 import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <!-- <h1>Hello world</h1> -->
   <Navbar/>
-  <TasksPage/>
+  <router-view/>
   <Footer/>
 </template>
 
