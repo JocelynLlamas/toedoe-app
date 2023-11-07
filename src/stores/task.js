c1574ca10f230724ff1f0b359d7bc6244864dcad
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2023-11-07 15:25:51 -0600

    Refactoring Stores

--- src/stores/task.js
+++ src/stores/task.js
@@ -1,28 +1,35 @@
 import { defineStore } from "pinia";
 import { allTasks } from "../http/task-api";
+import { computed, reactive, ref } from "vue";
 
 // Two arguments 
 // 1.Unique name:Is used by Pinia to connect the store to the view Dev tools
 // 2. An object option where we'll define state getters and actions (obj or function)
-export const useTaskStore = defineStore('taskStore', {
+
+const tmp = {
     state: () => ({
-        tasks: [],
-        task: {
-            id: 1,
-            name: "First task",
-            is_completed: false
-        },
+       
     }),
     getters: {
-        completedTasks: (state) => state.tasks.filter(task => task.is_completed),
-        uncompletedTasks() {
-            return this.tasks.filter(task => !task.is_completed)
-        }
+
     },
     actions: {
-        async fetchAllTasks() {
-            const {data} = await allTasks();
-            this.tasks = data.data;
-        },
+       
+    }
+}
+
+export const useTaskStore = defineStore('taskStore', ()=>{
+    const tasks = ref([])
+
+    const uncompletedTasks = computed(()=>tasks.value.filter(task => !task.is_completed))
+    const completedTasks = computed(()=>tasks.value.filter(task => task.is_completed))
+
+    const fetchAllTasks = async()=> {
+        const {data} = await allTasks();
+        tasks.value = data.data;
+    }
+
+    return {
+        tasks, completedTasks, uncompletedTasks, fetchAllTasks
     }
 })
