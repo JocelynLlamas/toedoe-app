2c9df7a0dd268ee31ce6ca0b59ddf1c835cd9c8d
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2023-11-10 13:24:52 -0600

    Building Login form

--- src/http/task-api.js
+++ src/http/task-api.js
@@ -1,8 +1,8 @@
 import api from "./api";
 
 
-const apiiPath = import.meta.env.VITE_API_PATH;
-const resource = apiiPath + "/tasks";
+const apiPath = import.meta.env.VITE_API_PATH;
+const resource = apiPath + "/tasks";
 
 export const allTasks = () => api.get(resource)
 
